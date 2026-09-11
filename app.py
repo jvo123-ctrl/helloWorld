93dbd428215b0ff4ff3a3ed31189a6017248de54
Jennifer <jvo123@umd.edu>
2026-09-11 15:14:21 -0400

    Adding first HTML page with Hello World message.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Jennifer Vo! I am adding my first code change'
 
-@app.route('/hello)
+@app.route('/hello')
 def hello():
     return render_template('hello.html')
 
